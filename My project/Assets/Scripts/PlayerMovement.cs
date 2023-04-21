f8e03c6ec45593d4b05e46ff24b2766275cb977f
Raks <raquelolho@hotmail.com>
2023-04-21 15:21:53 -0300

    updates

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Jumpforce = 1;
+    private bool canRun = true;
     public float scenarySpeed = 1;
+    public float playerSpeed = 40;
     public GameObject child;
     public bool IsGrounded = true;
 
@@ -20,13 +22,27 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("space") && IsGrounded == true)
-        { jump(); }
-        gameObject.transform.position += new Vector3(scenarySpeed, 0);
-
-
+        {
+            jump();
+        }
+        walk();
 
     }
 
+    void walk()
+    {
+        var axisX = Input.GetAxis("Horizontal");
+        if (canRun)
+        {
+            gameObject.transform.position += new Vector3(scenarySpeed, 0);
+
+        }
+        else
+        {
+            gameObject.transform.Translate(new Vector3(axisX * Time.deltaTime * playerSpeed, 0));
+
+        }
+    }
     void jump()
     {
 
@@ -42,8 +58,14 @@ public class PlayerMovement : MonoBehaviour
         {
             IsGrounded = true;
             child.GetComponent<SpriteRenderer>().sprite = walking;
-
-
+        }
+        if (other.gameObject.tag == "StartFreeZone")
+        {
+            canRun = false;
+        }
+        if (other.gameObject.tag == "EndFreeZone")
+        {
+            canRun = true;
         }
     }
 
